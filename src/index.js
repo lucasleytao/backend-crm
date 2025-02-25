6f6b4d9a4cf3edc7970aa306e9c806049da7b7a3
lucasleytao <lucassilvaleitao1401@gmail.com>
2025-02-25 16:06:15 -0300

    chore: Ajuste de pastas

--- src/index.js
+++ src/index.js
@@ -3,22 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './app/Context/auth';
 
-//conceito de jsx: mescla html e javascript
-
-// const nome = 'Lucas';
-// const snome = 'Leitao'
-
-//map: key:value
-
-// const estilo = {
-//   color:"tomato",
-//   fontSize:"30px"
-// }
-
 const root = ReactDOM.createRoot(document.getElementById('root')); //busca elemento id 'root'
-root.render( //renderiza na tela
-//o que sera renderizado deve estar envolvido em uma unica tag
-// envolve todo a aplicacao com o provedor
+root.render(
   <AuthProvider>
     <App />
   </AuthProvider>
